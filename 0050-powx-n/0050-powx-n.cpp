543d22a0183e44c2190da8ce28c7aa9e9ce3f6f1
Vaishnavigupta04 <guptavaishnavi682@gmail.com>
2026-08-19 23:59:36 +0530

    Time: 0 ms (100%), Space: 8.7 MB (44.62%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double power = pow(x,n);
+        long long N = n;
+        double power = 1;
+        if (n == 0)
+            return 1;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        while (N > 0) {
+            if (N % 2 == 1)
+                power = power * x;
+            x = x * x;
+            N = N / 2;
+        }
+
         return power;
-        
     }
 };
