7374354544f523ed0208068131dfd7b3eb7a6413
Vaishnavigupta04 <guptavaishnavi682@gmail.com>
2026-08-22 01:15:06 +0530

    Time: 0 ms (100%), Space: 10.4 MB (75.34%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1,
+                    int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+
+        int length = min(ay2, by2) - max(by1, ay1);
+        int breadth = min(bx2, ax2) - max(bx1, ax1);
+        if (length < 0 || breadth < 0) {
+            length = 0;
+            breadth = 0;
+        }
+        int carea = length * breadth;
+        int total = area1 + area2 - carea;
+        return total;
+    }
+};
