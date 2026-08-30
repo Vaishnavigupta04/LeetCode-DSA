cbb3209e9d6664ccc8984cad99ff6fc5476491b2
Vaishnavigupta04 <guptavaishnavi682@gmail.com>
2026-08-30 16:11:54 +0530

    Time: 0 ms (100%), Space: 92.2 MB (22.14%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int minimum = INT_MAX;
-        int maximum = INT_MIN;
-        int max_index, min_index;
+        int max_idx=0, min_idx=0;
         int n = nums.size();
 
         for (int i = 0; i < n; i++) {
-            if (maximum < nums[i]) {
-                maximum = nums[i];
-                max_index = i;
+            if (nums[max_idx] < nums[i]) {
+                max_idx = i;
             }
-            if (minimum > nums[i]) {
-                minimum = nums[i];
-                min_index = i;
+            if (nums[min_idx] > nums[i]) {
+                min_idx = i;
             }
         }
-        int d1 = max(max_index, min_index) + 1;
-        int d2 = n - min(max_index, min_index);
+        int d1 = max(max_idx, min_idx) + 1;
+        int d2 = n - min(max_idx, min_idx);
 
-        int d3 = min(max_index, min_index) + 1 + n - max(max_index, min_index);
+        int d3 = min(max_idx, min_idx) + 1 + n - max(max_idx, min_idx);
 
         return min({d1, d2, d3});
     }
