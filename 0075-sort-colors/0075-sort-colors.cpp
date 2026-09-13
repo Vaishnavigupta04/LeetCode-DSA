c3ff0ad90d3af44f2ec5ee877841914866e4b75d
Vaishnavigupta04 <guptavaishnavi682@gmail.com>
2026-09-14 00:08:49 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-
-        int n = nums.size();
-        int k = 0;
         int zeros = 0, ones = 0, twos = 0;
-
+        
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == 0)
                 zeros++;
